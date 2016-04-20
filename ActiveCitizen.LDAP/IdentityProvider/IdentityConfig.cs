9de8600900899f2a96d5188b934d0738051ef283
Michael Shipaev <Michael.Shipaev@gmail.com>
2016-04-20 11:30:03 +0300

    LDAP authentication working draft.

---
+++ ActiveCitizen.LDAP/IdentityProvider/IdentityConfig.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using System.DirectoryServices;
+
+namespace ActiveCitizen.LDAP.IdentityProvider
+{
+    // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        public ApplicationUserManager(IUserStore<ApplicationUser> store)
+            : base(store)
+        {
+        }
+
+        public override bool SupportsUserSecurityStamp
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public override bool SupportsUserClaim
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        private bool Authenticate(string userId, string password)
+        {
+            var connectionSettings = LdapConnectionSettings.GetDefaults();
+            string ldapfilter = "(&(objectclass=person)({0}={1}))";
+
+            try
+            {
+                string DN = "";
+                using (DirectoryEntry entry = new DirectoryEntry("LDAP://" + connectionSettings.Server + "/" + connectionSettings.BaseDN, connectionSettings.ServiceUserDN, connectionSettings.ServiceUserPassword, AuthenticationTypes.None))
+                {
+                    DirectorySearcher ds = new DirectorySearcher(entry);
+                    ds.SearchScope = SearchScope.Subtree;
+                    ds.Filter = string.Format(ldapfilter, connectionSettings.UserIdAttributeName == "DN" ? "entryDN" : connectionSettings.UserIdAttributeName, userId);
+                    SearchResult result = ds.FindOne();
+                    if (result != null)
+                    {
+                        DN = result.Path.Replace("LDAP://" + connectionSettings.Server + "/", "");
+                    }
+                }
+                // try logon   
+                try
+                {
+                    using (DirectoryEntry entry = new DirectoryEntry("LDAP://" + connectionSettings.Server + "/" + connectionSettings.BaseDN, DN, password, AuthenticationTypes.None))
+                    {
+                        DirectorySearcher ds = new DirectorySearcher(entry);
+                        ds.SearchScope = SearchScope.Subtree;
+                        SearchResult result = ds.FindOne();
+
+                        return true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex) { throw; }
+        }
+
+        protected override Task<bool> VerifyPasswordAsync(IUserPasswordStore<ApplicationUser, string> store, ApplicationUser user, string password)
+        {
+            return Task.FromResult<bool>(Authenticate(user.Id, password));
+        }
+
+        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context) 
+        {
+            var settings = LdapConnectionSettings.GetDefaults();
+
+            var manager = new ApplicationUserManager(new LdapUserStorage(context.Get<ApplicationDbContext>(), settings));
+            // Configure validation logic for usernames
+            manager.UserValidator = new UserValidator<ApplicationUser>(manager)
+            {
+                AllowOnlyAlphanumericUserNames = false,
+                RequireUniqueEmail = true
+            };
+
+            // Configure validation logic for passwords
+            manager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 6,
+                RequireNonLetterOrDigit = true,
+                RequireDigit = true,
+                RequireLowercase = true,
+                RequireUppercase = true,
+            };
+
+            // Configure user lockout defaults
+            manager.UserLockoutEnabledByDefault = false;
+            manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            manager.MaxFailedAccessAttemptsBeforeLockout = 5;
+
+            var dataProtectionProvider = options.DataProtectionProvider;
+            if (dataProtectionProvider != null)
+            {
+                manager.UserTokenProvider = 
+                    new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"));
+            }
+            return manager;
+        }
+    }
+
+    // Configure the application sign-in manager which is used in this application.
+    public class ApplicationSignInManager : SignInManager<ApplicationUser, string>
+    {
+        public ApplicationSignInManager(ApplicationUserManager userManager, IAuthenticationManager authenticationManager)
+            : base(userManager, authenticationManager)
+        {
+        }
+
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync(ApplicationUser user)
+        {
+            return user.GenerateUserIdentityAsync((ApplicationUserManager)UserManager);
+        }
+
+        public static ApplicationSignInManager Create(IdentityFactoryOptions<ApplicationSignInManager> options, IOwinContext context)
+        {
+            return new ApplicationSignInManager(context.GetUserManager<ApplicationUserManager>(), context.Authentication);
+        }
+    }
+}
---
+++ ActiveCitizen.LDAP/IdentityProvider/LdapUserStorage.cs
@@ -0,0 +1,230 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.DirectoryServices;
+using Microsoft.Owin.Security;
+using System.Security.Claims;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ActiveCitizen.LDAP.IdentityProvider
+{
+    public class LdapUserStorage : UserStore<ApplicationUser>
+        /*
+        IUserStore<ApplicationUser>
+        ,IUserPasswordStore<ApplicationUser, string>
+        , IUserLockoutStore<ApplicationUser, string>
+        ,IUserTwoFactorStore<ApplicationUser, string>
+        ,IUserPhoneNumberStore<ApplicationUser, string>
+        ,IUserLoginStore<ApplicationUser, string>
+        */
+    {
+        public LdapUserStorage(ApplicationDbContext context, LdapConnectionSettings connectionSettings) : base(context)
+        {
+            this.connectionSettings = connectionSettings;
+        }
+
+        private readonly LdapConnectionSettings connectionSettings;
+        
+        public override Task CreateAsync(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public override Task DeleteAsync(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+        /*
+        public override Task<ApplicationUser> FindByIdAsync(string userId)
+        {
+            var user = base.FindByIdAsync(userId).Result;
+
+            if (user != null) return Task.FromResult(user);
+
+            string ldapfilter = "(&(objectclass=person)({0}={1}))";
+
+            try
+            {
+                string DN = "";
+                using (DirectoryEntry entry = new DirectoryEntry("LDAP://" + connectionSettings.Server + "/" + connectionSettings.BaseDN, connectionSettings.ServiceUserDN, connectionSettings.ServiceUserPassword, AuthenticationTypes.None))
+                {
+                    DirectorySearcher ds = new DirectorySearcher(entry);
+                    ds.SearchScope = SearchScope.Subtree;
+                    ds.Filter = string.Format(ldapfilter, connectionSettings.UserIdAttributeName == "DN" ? "entryDN" : connectionSettings.UserIdAttributeName, userId);
+                    SearchResult result = ds.FindOne();
+                    if (result != null)
+                    {
+                        DN = result.Path.Replace("LDAP://" + connectionSettings.Server + "/", "");
+
+                        user = new ApplicationUser
+                        {
+                            Id =
+                            connectionSettings.UserIdAttributeName == "DN"
+                            ? DN
+                            : (string)result.Properties[connectionSettings.UserIdAttributeName][0],
+                            UserName = (string)result.Properties[connectionSettings.UserNameAttributeName][0]
+                        };
+                        this.CreateAsync(user).Wait();
+                        return base.FindByIdAsync(userId);
+                    }
+                }
+            }
+            catch (Exception ex) { Task.FromException(ex); }
+
+            return Task.FromResult<ApplicationUser>(null);
+        }
+        */
+        public override Task<ApplicationUser> FindByNameAsync(string userName)
+        {
+            var user = base.FindByNameAsync(userName).Result;
+
+            if (user != null) return Task.FromResult(user);
+
+            string ldapfilter = "(&(objectclass=person)({0}={1}))";
+
+            try
+            {
+                string DN = "";
+                using (DirectoryEntry entry = new DirectoryEntry("LDAP://" + connectionSettings.Server + "/" + connectionSettings.BaseDN, connectionSettings.ServiceUserDN, connectionSettings.ServiceUserPassword, AuthenticationTypes.None))
+                {
+                    DirectorySearcher ds = new DirectorySearcher(entry);
+                    ds.SearchScope = SearchScope.Subtree;
+                    ds.Filter = string.Format(ldapfilter, connectionSettings.UserNameAttributeName, userName);
+                    SearchResult result = ds.FindOne();
+                    if (result != null)
+                    {
+                        DN = result.Path.Replace("LDAP://" + connectionSettings.Server + "/", "");
+
+                        user = new ApplicationUser
+                        {
+                            Id =
+                            connectionSettings.UserIdAttributeName == "DN"
+                            ? DN
+                            : (string)result.Properties[connectionSettings.UserIdAttributeName][0],
+                            UserName = (string)result.Properties[connectionSettings.UserNameAttributeName][0]
+                        };
+
+                        base.CreateAsync(user).Wait();
+                        return base.FindByNameAsync(userName);
+                    }
+                }
+            }
+            catch (Exception ex) { Task.FromException(ex); }
+
+            return Task.FromResult<ApplicationUser>(null);
+        }
+        
+        public override Task UpdateAsync(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override  void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+        public override Task SetPasswordHashAsync(ApplicationUser user, string passwordHash)
+        {
+            return Task.CompletedTask;
+        }
+
+        public override Task<string> GetPasswordHashAsync(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<bool> HasPasswordAsync(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<DateTimeOffset> GetLockoutEndDateAsync(ApplicationUser user)
+        {
+            return Task.FromResult(DateTimeOffset.Now.AddYears(-1));
+        }
+
+        public override Task SetLockoutEndDateAsync(ApplicationUser user, DateTimeOffset lockoutEnd)
+        {
+            return Task.CompletedTask;
+        }
+
+        public override Task<int> IncrementAccessFailedCountAsync(ApplicationUser user)
+        {
+            return Task.FromResult(0);
+        }
+
+        public override Task ResetAccessFailedCountAsync(ApplicationUser user)
+        {
+            return Task.CompletedTask;
+        }
+
+        public override Task<int> GetAccessFailedCountAsync(ApplicationUser user)
+        {
+            return Task.FromResult(0);
+        }
+
+        public override Task<bool> GetLockoutEnabledAsync(ApplicationUser user)
+        {
+            return Task.FromResult(false);
+        }
+
+        public override Task SetLockoutEnabledAsync(ApplicationUser user, bool enabled)
+        {
+            return Task.CompletedTask;
+        }
+
+        public override Task SetTwoFactorEnabledAsync(ApplicationUser user, bool enabled)
+        {
+            return Task.CompletedTask;
+        }
+
+        public override Task<bool> GetTwoFactorEnabledAsync(ApplicationUser user)
+        {
+            return Task.FromResult(false);
+        }
+
+        public override Task SetPhoneNumberAsync(ApplicationUser user, string phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<string> GetPhoneNumberAsync(ApplicationUser user)
+        {
+            return Task.FromResult("");
+        }
+
+        public override Task<bool> GetPhoneNumberConfirmedAsync(ApplicationUser user)
+        {
+            return Task.FromResult(true);
+        }
+
+        public override Task SetPhoneNumberConfirmedAsync(ApplicationUser user, bool confirmed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task AddLoginAsync(ApplicationUser user, UserLoginInfo login)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task RemoveLoginAsync(ApplicationUser user, UserLoginInfo login)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<IList<UserLoginInfo>> GetLoginsAsync(ApplicationUser user)
+        {
+            return Task.FromResult((IList<UserLoginInfo>)(new List<UserLoginInfo> { new UserLoginInfo("LDAP", user.Id) }));
+        }
+
+        public override Task<ApplicationUser> FindAsync(UserLoginInfo login)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
